cf1c03663360da835c9328f5759188352ec5ae31
almax07082005 <almaxim07082005@gmail.com>
2024-06-25 20:24:30 +0300

    add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'default': {
+          '50': '#effef4',
+          '100': '#d9ffe5',
+          '200': '#b5fdcc',
+          '300': '#7cf9a6',
+          '400': '#3cec78',
+          '500': '#13d455',
+          '600': '#09b043',
+          '700': '#0b8937',
+          '800': '#0f6c30',
+          '900': '#0e592a',
+          '950': '#013214',
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
