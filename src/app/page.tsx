1b9b79e8e10168524e209114fb4fd99b3ebdd6ff
Aleliya <168584779+Aleliya@users.noreply.github.com>
2024-07-02 18:51:57 +0300

    hero component
    change img to Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/components/navbar";
-
 export default function Home() {
   return (
       <div>
-        <Navbar text1={"Aleliya"} text2={"ok"} text3={"js"}></Navbar>
       </div>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image'
-import profilePic from '../colorimagehero.png'
+
 
 
 export default function Hero() {
     return (
         <section className="bg-white">
 
-            <img className="h-auto w-full object-cover"
-                 src="../colorimagehero.png" alt="Italian landscape"/>
+            <Image width={1920} height={1080} className="h-auto w-full object-cover"
+                 src="/colorimagehero.png" alt="Italian landscape"/>
             <div
-                    className="z-10 mb-44 absolute inset-0 flex items-center justify-center text-4xl font-serif tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+                    className="z-10 mb-44 absolute inset-0 flex items-center justify-center text-4xl font-serif tracking-tight leading-none text-default-50 md:text-5xl lg:text-6xl">
                     <h1>Your window to sunny Italy</h1>
                 </div>
-                <div className="z-10 absolute inset-0 flex items-center justify-center text-lg font-normal text-white lg:text-2xl">
+                <div className="z-10 absolute inset-0 flex items-center justify-center text-lg font-normal text-default-50 lg:text-2xl">
                     <p>Nulla è sprecato, se fatto con amore.</p>
             </div>
 
