c5678e07c4657eefa234839f8da70a58ac7b951e
Arina Petuhova <119685834+arinapetukhova@users.noreply.github.com>
2024-07-02 19:58:52 +0300

    fix: did pull request review comments
    Fixed Section Our Story according to the comments from the pull request review

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -1,14 +1,22 @@
+import Image from 'next/image'
+import pastaEgg from '/public/pastaEgg.jpg'
+import pasta from '/public/pasta.jpg'
+import pastaReady from '/public/pastaReady.jpg'
+
 export default function Story() {
     return (
         <div>
             <section
-                className="@apply flex min-h-screen flex-col items-center justify-center bg-fixed bg-[url('https://wallpaper.forfun.com/fetch/5b/5b58f53e0636f0e18b4caab18f1aa017.jpeg')] bg-center bg-cover text-black] gap-y-16 text-center">
-                <h1 className="text-lime-200 text-6xl font-serif mb-[-10%]">Pasta Palace</h1>
+                className="@apply flex min-h-screen flex-col items-center justify-center bg-fixed bg-[url('/palace.jpeg')] bg-center bg-cover text-black] gap-y-16 text-center">
+                <div className="w-2xl bg-red-900 opacity-95 border border-red-900 shadow dark:bg-gray-800 dark:border-gray-700 m-auto pt-4 pb-4 pl-24 pr-24">
+                    <h1 className="text-white text-8xl font-serif">Pasta
+                        Palace</h1>
+                </div>
             </section>
 
-            <section className="@apply w-full min-h-[55vh] bg-red-900"><br/>
-                <h1 className="text-center text-5xl text-lime-200 font-serif">Our story</h1><br/>
-                <p className="ml-80 text-center font-serif w-[55%] text-2xl text-lime-200">
+            <section className="@apply w-full min-h-[50vh] bg-red-900"><br/>
+                <h1 className="text-center text-5xl text-white font-serif">Our story</h1><br/>
+                <p className="m-auto text-center font-serif w-[55%] text-2xl text-white">
                     Welcome to Pasta Palace, a cozy and inviting eatery serving up delicious Italian-inspired dishes.
                     Our
                     story began with a passion for pasta and a dream to share that love with our community. Our chefs
@@ -23,9 +31,9 @@ export default function Story() {
             </section>
 
             <section
-                className="@apply bg-gray-50/40 flex min-h-screen flex-col items-center justify-center bg-fixed bg-[url('https://facts.net/wp-content/uploads/2023/11/11-interesting-italian-food-facts-1700670149.jpg')] bg-center bg-cover text-black] gap-y-16 text-center bg-white">
+                className="@apply bg-gray-50/40 flex min-h-screen flex-col items-center justify-center bg-fixed bg-[url('/italianFood.jpg')] bg-center bg-cover text-black] gap-y-16 text-center bg-white">
                 <div
-                    className="max-w-2xl bg-lime-200 border border-lime-200 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700 ml-96">
+                    className="max-w-2xl bg-yellow-100 border border-yellow-100 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700 ml-96">
                     <div className="p-5">
                         <h3 className="text-4xl font-serif text-red-900">Our goal</h3><br/>
                         <p className="mb-3 font-normal text-red-900 dark:text-gray-400 font-serif text-xl">At Pasta
@@ -37,7 +45,7 @@ export default function Story() {
                 </div>
 
                 <div
-                    className="max-w-2xl bg-lime-200 border border-lime-200 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700 mr-96 mt-24">
+                    className="max-w-2xl bg-yellow-100 border border-yellow-100 rounded-3xl shadow dark:bg-gray-800 dark:border-gray-700 mr-96 mt-24">
                     <div className="p-5">
                         <h3 className="text-4xl font-serif text-red-900">Our purpose</h3><br/>
                         <p className="mb-3 font-normal text-red-900 dark:text-gray-400 font-serif text-xl">Beyond
@@ -50,18 +58,18 @@ export default function Story() {
                 </div>
             </section>
 
-            <section className="@apply w-full min-h-[100vh] bg-orange-700 bg-opacity-90"><br/>
-                <h1 className=" text-center text-5xl text-lime-200 font-serif">Our kitchen</h1><br/>
-                <p className="ml-80 text-center font-serif w-[55%] text-2xl text-lime-200">
+            <section className="@apply w-full min-h-[100vh] bg-red-900"><br/>
+                <h1 className=" text-center text-5xl text-white font-serif">Our kitchen</h1><br/>
+                <p className="m-auto text-center font-serif w-[55%] text-2xl text-white">
                     Step into our kitchen and you'll find a team of passionate chefs who pour their heart and soul into
                     each and every dish. Using only the freshest ingredients and time-honored recipes, we craft
                     mouthwatering pasta dishes that will tantalize your taste buds and leave you craving more.
                 </p>
                 <div className="@apply w-full min-h-[61vh] bg-orange-700 bg-opacity-0 text-center">
-                    <img className="rounded-full w-96 h-96 justify-center mt-8 m-auto"
-                         src="../pastaEgg.jpg"
+                    <Image className="rounded-full w-96 h-96 justify-center mt-8 m-auto"
+                         src={pastaEgg}
                          alt="Pasta dough making"/><br/>
-                    <p className="ml-80 text-center font-serif w-[55%] text-2xl text-lime-200 mb-16">
+                    <p className="m-auto text-center font-serif w-[55%] text-2xl text-white mb-16">
                         We take pride in serving the finest quality of food that truly captures the essence of
                         traditional Italian cuisine. Our experienced chefs meticulously select the freshest ingredients
                         to craft each dish with care and precision.
@@ -69,19 +77,19 @@ export default function Story() {
                 </div>
 
                 <div className="@apply w-full min-h-[78vh] bg-yellow-50 text-center"><br/>
-                    <img className="rounded-full w-96 h-96 mt-12 m-auto"
-                         src="../pasta.jpg"
+                    <Image className="rounded-full w-96 h-96 mt-12 m-auto"
+                         src={pasta}
                          alt="Pasta cutting"/><br/>
-                    <p className="ml-80 text-center font-serif w-[55%] text-2xl text-grey-700">
+                    <p className="m-auto text-center font-serif w-[55%] text-2xl text-grey-700">
                         With a focus on authentic recipes and time-honored cooking techniques, every bite is a symphony
                         of flavors that transport you to the streets of Italy.
                     </p>
                 </div>
                 <div className="@apply w-full min-h-[75vh] bg-lime-200"><br/>
-                    <img className="rounded-full w-96 h-96 mt-12 m-auto"
-                         src="../pastaReady.jpg"
+                    <Image className="rounded-full w-96 h-96 mt-12 m-auto"
+                         src={pastaReady}
                          alt="Pasta and ravioli on a table"/><br/>
-                    <p className="ml-80 text-center font-serif w-[55%] text-2xl text-red-900">
+                    <p className="m-auto text-center font-serif w-[55%] text-2xl text-red-900">
                         From our homemade pasta to wood-fired pizzas, each dish is a celebration of taste that will
                         leave you craving more. Come savor the true essence of Italy with us and experience the
                         unparalleled quality and deliciousness of our dishes.
@@ -90,7 +98,7 @@ export default function Story() {
             </section>
 
             <section
-                className="@apply flex min-h-screen flex-col items-center justify-center bg-fixed bg-[url('https://orders.co/static/44cbe207cad4090751ef79c87c04395e/f659b/Selecting-the-Perfect-Location-for-Your-Italian-Eatery.png')] bg-center bg-cover text-black] gap-y-16 text-center opacity-70">
+                className="@apply flex min-h-screen flex-col items-center justify-center bg-fixed bg-[url('/restaurant.jpg')] bg-center bg-cover text-black] gap-y-16 text-center opacity-70">
                 <h1 className="text-5xl text-lime-200 font-serif align-text-top">Our atmosphere</h1><br/>
             </section>
         </div>
