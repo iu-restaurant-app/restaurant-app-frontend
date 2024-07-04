075ce663ebbb9d52d85d9385ce5fe0224ca7e519
mituttta051 <amityutneva@mail.ru>
2024-07-04 22:03:11 +0300

    feat: get method in MealService

--- src/api/meal/service/meal-service.ts
+++ src/api/meal/service/meal-service.ts
@@ -1,3 +1,22 @@
-export default class MealService {
+import axios from "axios";
+import {MealCreateRequest} from "@/api/meal/request/meal-create-request";
 
+export default class MealService {
+    static readonly MEAL_SERVICE_BASE_URL = "http://10.90.137.110:8080/meal";
+
+    static get(title: string) {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'get',
+                baseURL: this.MEAL_SERVICE_BASE_URL,
+                url: "/" + title
+            })
+                .then((response) => {
+                    resolve(response.data as MealCreateRequest)
+            })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+    }
 }
