116ba986fd093aa025eb803ef7403632bcda6218
almax07082005 <almaxim07082005@gmail.com>
2024-07-08 12:18:42 +0300

    feat: menu-gallery added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+
+import MenuGallery from '@/components/menu-gallery';
+import { useEffect, useState } from 'react';
+import { MealRequestResponse } from '@/api/meal/request/meal-request-response';
+import MealService from '@/api/meal/service/meal-service';
+import ByteArrayToImage from '@/utils/byte-array-to-image';
+
 export default function Home() {
-  return <div></div>;
+  const [meals, setMeals] = useState([] as MealRequestResponse[]);
+  useEffect(() => {
+    MealService.getAll()
+      .then(response => {
+        response.forEach(item => {
+          item.image = ByteArrayToImage(item.image);
+        });
+        setMeals(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return <MenuGallery items={[...meals]} />;
 }
