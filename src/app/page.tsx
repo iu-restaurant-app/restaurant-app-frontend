4d608e5597ec0cd0f3d20dcafc797983824723ef
almax07082005 <almaxim07082005@gmail.com>
2024-07-09 09:18:37 +0300

    fix: api getter location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import MealService from '@/api/meal/service/meal-service';
-import ByteArrayToImage from '@/utils/byte-array-to-image';
-import { MealRequestResponse } from '@/api/meal/request/meal-request-response';
-import MenuGallery from '@/components/menu-gallery';
-import ScrollToTopButton from '@/components/scroll-to-top-button';
-
 export default function Home() {
-  const [meals, setMeals] = useState([] as MealRequestResponse[]);
-  useEffect(() => {
-    MealService.getAll()
-      .then(response => {
-        response.forEach(item => {
-          item.image = ByteArrayToImage(item.image);
-        });
-        setMeals(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <MenuGallery items={[...meals]} />
-      <ScrollToTopButton />
-    </div>
-  );
+  return <div></div>;
 }
--- src/components/menu-gallery.tsx
+++ src/components/menu-gallery.tsx
@@ -1,13 +1,29 @@
+'use client';
+
 import MenuItem, { MenuItemProps } from '@/components/menu-item';
+import { useEffect, useState } from 'react';
+import { MealRequestResponse } from '@/api/meal/request/meal-request-response';
+import MealService from '@/api/meal/service/meal-service';
+import ByteArrayToImage from '@/utils/byte-array-to-image';
 
-interface MenuGalleryProps {
-  items: MenuItemProps[];
-}
+export default function MenuGallery() {
+  const [meals, setMeals] = useState([] as MealRequestResponse[]);
+  useEffect(() => {
+    MealService.getAll()
+      .then(response => {
+        response.forEach(item => {
+          item.image = ByteArrayToImage(item.image);
+        });
+        setMeals(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-export default function MenuGallery(props: MenuGalleryProps) {
   return (
     <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-[100px]">
-      {props.items.map((item: MenuItemProps, index: number) => (
+      {meals.map((item: MenuItemProps, index: number) => (
         <MenuItem
           key={index}
           title={item.title}
