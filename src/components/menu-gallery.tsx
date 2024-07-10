391f12cdc16c1746bdebbd14cd365a4e96050c1f
almax07082005 <almaxim07082005@gmail.com>
2024-07-10 15:28:32 +0300

    fix: error page

--- src/components/menu-gallery.tsx
+++ src/components/menu-gallery.tsx
@@ -50,11 +50,11 @@ export default function MenuGallery() {
             <div
               key={index}
               role="status"
-              className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700"
+              className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6"
             >
-              <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+              <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded">
                 <svg
-                  className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                  className="w-10 h-10 text-gray-200"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -64,13 +64,13 @@ export default function MenuGallery() {
                   <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
                 </svg>
               </div>
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4"></div>
+              <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
+              <div className="h-2 bg-gray-200 rounded-full "></div>
               <div className="flex items-center mt-4">
                 <svg
-                  className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700"
+                  className="w-10 h-10 me-3 text-gray-200"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -79,8 +79,8 @@ export default function MenuGallery() {
                   <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
                 </svg>
                 <div>
-                  <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-                  <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                  <div className="h-2.5 bg-gray-200 rounded-full w-32 mb-2"></div>
+                  <div className="w-48 h-2 bg-gray-200 rounded-full"></div>
                 </div>
               </div>
               <span className="sr-only">Loading...</span>
@@ -89,22 +89,21 @@ export default function MenuGallery() {
         </div>
       )) ||
         (pageState === PageState.ERROR && (
-          <section className="bg-white dark:bg-gray-900">
+          <section className="bg-white">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
               <div className="mx-auto max-w-screen-sm text-center">
-                <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+                <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-red-600">
                   {error}
                 </h1>
-                <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+                <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
                   Something is missing.
                 </p>
-                <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-                  Sorry, we cannot find that page. You will find lots to explore
-                  on the home page.{' '}
+                <p className="mb-4 text-lg font-light text-gray-500">
+                  Sorry, something went wrong. Try again later.{' '}
                 </p>
                 <a
                   href="/"
-                  className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+                  className="inline-flex text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
                 >
                   Back to Homepage
                 </a>
