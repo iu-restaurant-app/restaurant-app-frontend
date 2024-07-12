364074818140d13310aaf888dced7e5a11c7e954
Arina Petuhova <119685834+arinapetukhova@users.noreply.github.com>
2024-07-12 17:54:12 +0300

    feat: SEO component added
    SEO component was added to the main page and about-us page, in the layout title and description were changed

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,18 @@
+'use client';
 import ContactUs from '@/components/about-us/contact-us';
 import ScrollToTopButton from '@/components/common/scroll-to-top-button';
 import Hero from '@/components/about-us/hero';
 import Story from '@/components/about-us/story';
+import SEO from '@/components/common/seo';
+import React from 'react';
 
 export default function Home() {
   return (
     <>
+      <SEO
+        title={'About Pasta Palace'}
+        description={'Page with the story of Pasta Palace creation'}
+      />
       <Hero />
       <Story />
       <ContactUs />
