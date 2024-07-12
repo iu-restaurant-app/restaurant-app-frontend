876dd685435baff7df8c99757aad9b7fddc685fb
almax07082005 <almaxim07082005@gmail.com>
2024-07-12 17:43:35 +0300

    fix: devDependencies

--- src/components/menu/menu-gallery.tsx
+++ src/components/menu/menu-gallery.tsx
@@ -104,7 +104,8 @@ export default function MenuGallery() {
                   Sorry, something went wrong. Try again later.{' '}
                 </p>
                 <a
-                  href="/public"
+                  href="/"
+                  type="submit"
                   className="inline-flex text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
                 >
                   Back to Homepage
