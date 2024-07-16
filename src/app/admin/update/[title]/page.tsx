f082f6b0e6d7a5a96fc8e92a5e3d5930bf14d1fe
Aleliya <168584779+Aleliya@users.noreply.github.com>
2024-07-16 13:07:41 +0300

    fix: added component that accepts value

--- src/app/admin/update/[title]/page.tsx
+++ src/app/admin/update/[title]/page.tsx
@@ -1,114 +1,29 @@
+'use client';
 import React from 'react';
+import { usePathname } from 'next/navigation';
+import InputForm from '@/components/admin/input-form-meal';
 
-export default function UpdatePage(title: string) {
+function Page() {
+  const pathname = usePathname();
+  const segments = pathname.split('/');
+  const title = segments[segments.length - 1];
+
+  if (!title) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div className="flex justify-center items-center min-h-screen bg-default-50">
-      <div className="w-full max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-        <form className="space-y-6" action="#">
-          <h5 className="text-2xl font-medium text-gray-900 dark:text-white">
-            Update or add meal
-          </h5>
-          <>
-            <label className="block mb-4 text-base font-medium text-gray-900 ">
-              Photo
-            </label>
-            <div className="flex items-center justify-center w-full">
-              <label
-                htmlFor="dropzone-file"
-                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
-              >
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG or JPG
-                  </p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-            </div>
-          </>
-          <div>
-            <label
-              htmlFor="title"
-              className="block mb-4 text-base font-medium text-gray-900 "
-            >
-              Title
-            </label>
-            <input
-              type="text"
-              id="default-title"
-              className="block w-full p-4 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-default-600 focus:border-default-600 outline-0"
-              placeholder="Pizza Margarita"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="block mb-4 text-base font-medium text-gray-900"
-            >
-              Description
-            </label>
-            <textarea
-              id="message"
-              className="block p-2.5 w-full text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-default-600 focus:border-default-600 outline-0"
-              placeholder="Write description for meal here..."
-            ></textarea>
-          </div>
-          <div>
-            <label className="block mb-4 text-base font-medium text-gray-900 ">
-              Calories
-            </label>
-            <input
-              type="text"
-              id="calories"
-              className="block w-full p-4 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-default-600 focus:border-default-600 outline-0"
-              placeholder="273 cal."
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="number-input"
-              className="block mb-4 text-base font-medium text-gray-900"
-            >
-              Price
-            </label>
-            <input
-              type="number"
-              id="number-input"
-              aria-describedby="helper-text-explanation"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-default-600 focus:border-default-600 block w-full p-2.5 outline-0"
-              placeholder="130"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full text-white bg-default-700 hover:bg-default-800 focus:ring-4 focus:outline-none focus:ring-default-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-default-600 dark:hover:bg-default-700 dark:focus:ring-default-800"
-          >
-            Save changes
-          </button>
-        </form>
-      </div>
-    </div>
+    <>
+      <InputForm
+        formTitle={`Update ${title}`}
+        initialTitle={title as string}
+        initialDescription={''}
+        initialPrice={0}
+        initialImage={''}
+        initialCalories={0}
+        key={0}
+      ></InputForm>
+    </>
   );
 }
+
+export default Page;
---
+++ src/components/admin/input-form-meal.tsx
@@ -0,0 +1,133 @@
+'use client';
+import React from 'react';
+
+interface FormItemProps {
+  formTitle: string;
+  initialTitle: string;
+  initialDescription: string;
+  initialPrice: number;
+  initialCalories: number;
+  initialImage: string;
+}
+
+export default function UpdatePage({
+  formTitle,
+  initialTitle = '',
+  initialDescription = '',
+  initialPrice,
+  initialCalories = 0,
+  initialImage = '',
+}: FormItemProps) {
+  const [title, setTitle] = React.useState(initialTitle);
+  const [description, setDescription] = React.useState(initialDescription);
+  const [price, setPrice] = React.useState(initialPrice);
+  const [calories, setCalories] = React.useState(initialCalories);
+  const [image, setImage] = React.useState(initialImage);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-default-50">
+      <div className="w-full max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
+        <form onSubmit={handleSubmit} className="space-y-6" action="#">
+          <h5 className="text-2xl font-medium text-gray-900 dark:text-white">
+            {formTitle}
+          </h5>
+          <>
+            <label
+              htmlFor="photo_input"
+              className="block mb-4 text-base font-medium text-gray-900 "
+            >
+              Photo
+            </label>
+            <input
+              src={image}
+              onChange={e => setImage(e.target.value)}
+              className="block w-full p-0.5 text-base text-gray-900 border border-gray-300 rounded-md cursor-pointer bg-gray-50 outline-0"
+              aria-describedby="file_input_help"
+              id="photo_input"
+              type="file"
+            ></input>
+          </>
+          <div>
+            <label
+              htmlFor="title"
+              className="block mb-4 text-base font-medium text-gray-900 "
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              id="default-title"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              className="block w-full p-4 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-default-600 focus:border-default-600 outline-0"
+              placeholder="Pizza Margarita"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="block mb-4 text-base font-medium text-gray-900"
+            >
+              Description
+            </label>
+            <textarea
+              id="message"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              className="block p-2.5 w-full text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-default-600 focus:border-default-600 outline-0"
+              placeholder="Write description for meal here..."
+              maxLength={100}
+              required
+            ></textarea>
+          </div>
+          <div>
+            <label
+              htmlFor="number-input"
+              className="block mb-4 text-base font-medium text-gray-900"
+            >
+              Calories
+            </label>
+            <input
+              type="number"
+              id="number-input"
+              value={calories}
+              onChange={e => setCalories(Number(e.target.value))}
+              aria-describedby="helper-text-explanation"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-default-600 focus:border-default-600 block w-full p-2.5 outline-0"
+              placeholder="370"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="number-input"
+              className="block mb-4 text-base font-medium text-gray-900"
+            >
+              Price
+            </label>
+            <input
+              type="number"
+              id="number-input"
+              value={price}
+              onChange={e => setPrice(Number(e.target.value))}
+              aria-describedby="helper-text-explanation"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-default-600 focus:border-default-600 block w-full p-2.5 outline-0"
+              placeholder="130"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full text-white bg-default-700 hover:bg-default-800 focus:ring-4 focus:outline-none focus:ring-default-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Save changes
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
